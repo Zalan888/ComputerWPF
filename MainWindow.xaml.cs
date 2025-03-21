6e4c9ea8dd090a135a2221778fdcb4e27e2f55b7
Zalan888 <witmannz@kkszki.hu>
2025-03-21 12:18:24 +0100

    Added some simple UI elements, and added functions for handilg select queries

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySqlConnector;
 
 namespace Computer
 {
@@ -19,6 +21,68 @@ namespace Computer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public const string connectionString = "Server=localhost;User ID=root;Password=;Database=computer";
+
+        private void systemsButton_Click(object sender, RoutedEventArgs e)
+        {
+            myListBox.Items.Clear();
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var query = "SELECT * FROM osystem";
+                    using (var cmd = new MySqlCommand(query, connection))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        { 
+                            while (reader.Read())
+                            {
+                                myListBox.Items.Add("ID: " + reader.GetInt32(0) + " Name: " + reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(Convert.ToString(error));
+                throw;
+            }
+        }
+
+        private void computersButton_Click(object sender, RoutedEventArgs e)
+        {
+            myListBox.Items.Clear();
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var query = "SELECT * FROM comp join osystem on comp.osId = osystem.id;";
+                    using (var cmd = new MySqlCommand(query, connection))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string createdTime = reader.IsDBNull(5) == false ? Convert.ToString(reader.GetDateTime(5)) : "0000-00-00 00:00:00";
+
+                                myListBox.Items.Add("ID: " + reader.GetInt32(0) + " Brand: " + reader.GetString(1) + " Type: " + reader.GetString(2) + " Display: " + reader.GetDouble(3) + " Memory: " + reader.GetInt32(4) + " CreatedTime: " + createdTime + " OSID: " + reader.GetInt32(6) + " OS: " + reader.GetString(8));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(Convert.ToString(error));
+                throw;
+            }
         }
     }
 }
